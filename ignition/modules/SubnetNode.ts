ca4c47895f41e2ffbc2960d01e2d206724515dc4
Nguyen Thang <thangn.1411@gmail.com>
2024-11-12 17:44:04 +0700

    first commit

---
+++ ignition/modules/SubnetNode.ts
@@ -0,0 +1,15 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const JAN_1ST_2030 = 1893456000;
+const ONE_GWEI: bigint = 1_000_000_000n;
+
+const LockModule = buildModule("SubnetNode", (m) => {
+  const lock = m.contract("SubnetNode", [], {});
+
+  return { lock };
+});
+
+export default LockModule;
