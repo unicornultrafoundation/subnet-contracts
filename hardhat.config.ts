5b3eabcf68413863cd4eeb86457202e617b31a57
thang <thangn.1411@gmail.com>
2024-12-10 16:06:09 +0700

    deploy testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,47 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import "@nomicfoundation/hardhat-verify";
+
+const privateKey = process.env.PRIVATE_KEY
 
 const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+    },
+    nebulas: {
+      url: "https://rpc-nebulas-testnet.u2u.xyz",
+      accounts: privateKey ? [privateKey] : []
+    },
+    u2u: {
+      url: "https://rpc-mainnet.u2u.xyz",
+      accounts: privateKey ? [privateKey] : []
+    }
+  },
+  etherscan: {
+    apiKey: {
+      nebulas: "1",
+      u2u: "1"
+    },
+    customChains: [
+      {
+        network: "nebulas",
+        chainId: 2484,
+        urls: {
+          apiURL: "https://testnet.u2uscan.xyz/api",
+          browserURL: "https://testnet.u2uscan.xyz",
+        }
+      },
+      {
+        network: "u2u",
+        chainId: 39,
+        urls: {
+          apiURL: "https://u2uscan.xyz/api",
+          browserURL: "https://u2uscan.xyz",
+        }
+      }
+    ]
+  },
   solidity: {
     version: "0.8.20",
     settings: {
--- ignition/modules/SubnetRegistry.ts
+++ ignition/modules/SubnetRegistry.ts
@@ -3,14 +3,17 @@
 
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-// Define the deployment module for SubnetRegistry
-const SubnetRegistryModule = buildModule("SubnetRegistry", (m) => {
-  const nftContractAddress = "0xYourNFTContractAddress"; // Replace with your NFT contract address
-  const ownerAddress = "0x"; // Replace with your owner address
-  const rewardPerSecond = 1_000_000_000_000_000n; // Reward rate in wei (e.g., 0.001 ETH per second)
 
-  // Deploy the SubnetRegistry contract
-  const subnetRegistry = m.contract("SubnetRegistry", [ownerAddress, nftContractAddress, rewardPerSecond]);
+
+const SubnetRegistryModule = buildModule("SubnetRegistry", (m) => {
+  // Constants
+  const NFT_CONTRACT_ADDRESS = "0x9CEb5fBb9734aC71F365d7cc0EedA48bB9763891"; // Replace with your NFT contract address
+  const REWARD_PER_SECOND = 1000000000000000n; // Example: 0.001 ether per second
+
+  const subnetRegistry = m.contract(
+    "SubnetRegistry", 
+    ["0x8cb3C64E938065b1Ab4b137dB2b0e9953f66c3Eb", NFT_CONTRACT_ADDRESS, REWARD_PER_SECOND], // Constructor arguments
+  );
 
   return { subnetRegistry };
 });
